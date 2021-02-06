5e80f46db8ed9f4d976dfdbfc8002939073889ed
SeckinBakir <seckin.bakir07@gmail.com>
2021-02-06 23:25:01 +0300

    06.02.2021 Kurumsal Mimari

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,6 +9,16 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            //ProdcutTest();
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProdcutTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
             foreach (var product in productManager.GetAllByCategory(2))
             {
--- DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -16,11 +16,11 @@ namespace DataAccess.Concrete.InMemory
         {
             _products = new List<Product>
             {
-                new Product{CategoryId = 1,ProductId = 1,ProductName = "Bardak",UnitInStock = 15,UnitPrice = 15},
-                new Product{CategoryId = 1, ProductId = 2, ProductName = "Kamera",UnitInStock = 3,UnitPrice = 500},
-                new Product{CategoryId = 1,ProductId = 3,ProductName = "Telefon",UnitInStock = 15,UnitPrice = 1500},
-                new Product{CategoryId = 1,ProductId = 4, ProductName = "Klavye", UnitInStock = 15,UnitPrice = 150},
-                new Product {CategoryId = 1,ProductId = 5, ProductName = "Fare",UnitInStock = 1,UnitPrice = 85}
+                new Product{CategoryId = 1,ProductId = 1,ProductName = "Bardak",UnitsInStock = 15,UnitPrice = 15},
+                new Product{CategoryId = 1, ProductId = 2, ProductName = "Kamera",UnitsInStock = 3,UnitPrice = 500},
+                new Product{CategoryId = 1,ProductId = 3,ProductName = "Telefon",UnitsInStock = 15,UnitPrice = 1500},
+                new Product{CategoryId = 1,ProductId = 4, ProductName = "Klavye", UnitsInStock = 15,UnitPrice = 150},
+                new Product {CategoryId = 1,ProductId = 5, ProductName = "Fare",UnitsInStock = 1,UnitPrice = 85}
             };
         }
         public void Add(Product product)
@@ -69,7 +69,7 @@ namespace DataAccess.Concrete.InMemory
             Product producToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
             producToUpdate.ProductName = product.ProductName;
             producToUpdate.CategoryId = product.CategoryId;
-            producToUpdate.UnitInStock = product.UnitInStock;
+            producToUpdate.UnitsInStock = product.UnitsInStock;
             producToUpdate.UnitPrice = product.UnitPrice;
         }
     }
